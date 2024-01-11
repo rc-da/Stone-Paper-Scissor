f13c1c403095db13f112550d81a3a1614d12f2da
AADITYA <=>
2024-01-12 00:26:11 +0530

    stonePaper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [isGame, setIsGame] = useState(true);
+  const [isGameOn, setIsGameOn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!isGameOn ? (
+        <GameCard>
+          {isGame ? "Welcome to Stone Paper !!" : "Would Like Play ??"}
+          <span>
+            <Button onClick={() => setIsGameOn((game) => !game)}>
+              {isGame ? "Start" : "Start again"}
+            </Button>
+          </span>
+        </GameCard>
+      ) : (
+        <Game setIsGame={setIsGame} setIsGameOn={setIsGameOn} />
+      )}
     </div>
   );
 }
 
-export default App;
+function GameCard({ children }) {
+  return (
+    <div className="game-card">
+      <p>{children}</p>
+    </div>
+  );
+}
+
+function Game({ setIsGame, setIsGameOn }) {
+  const [userChoice, setUserChoice] = useState(0);
+  const [comChoice, setComChoice] = useState(0);
+  const [state, setState] = useState("Start Game");
+  const [count, setCount] = useState(0);
+  const [userPoint, setUserPoint] = useState(0);
+  const [comPoint, setCompoint] = useState(0);
+
+  function computerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber;
+  }
+
+  function handleButton(choice) {
+    const newComChoice = computerChoice();
+    setUserChoice((userChoice) => choice);
+    setComChoice((comChoice) => newComChoice);
+    setCount((c) => c + 1);
+
+    if (choice === newComChoice) setState("It's a Tie");
+    else if (
+      (choice === 1 && newComChoice === 0) ||
+      (choice === 0 && newComChoice === 2)
+    ) {
+      setState("You won 1 point");
+      setUserPoint((userPoint) => userPoint + 1);
+    } else {
+      setState("Computer won 1 point");
+      setCompoint((comPoint) => comPoint + 1);
+    }
+  }
+
+  return count < 5 ? (
+    <div className="game">
+      <p className="p">{state}</p>
+      <div className="content">
+        <p>
+          You {userPoint} points
+          <span>
+            {userChoice === 0 ? (
+              <img src="./img/scissor.png" alt="scissor" />
+            ) : userChoice === 1 ? (
+              <img src="./img/stone.png" alt="stone" />
+            ) : (
+              <img src="./img/paper.png" alt="paper" />
+            )}
+          </span>
+        </p>
+
+        <p>
+          Computer {comPoint} points
+          <span>
+            {comChoice === 0 ? (
+              <img src="./img/scissor.png" alt="scissor" />
+            ) : comChoice === 1 ? (
+              <img src="./img/stone.png" alt="stone" />
+            ) : (
+              <img src="./img/paper.png" alt="paper" />
+            )}
+          </span>
+        </p>
+      </div>
+
+      <div className="buttons">
+        <Button onClick={() => handleButton(1)}>Stone</Button>
+        <Button onClick={() => handleButton(2)}>Paper</Button>
+        <Button onClick={() => handleButton(0)}>Scissor</Button>
+      </div>
+    </div>
+  ) : (
+    <>
+      {userPoint > comPoint
+        ? "You Won !"
+        : userPoint < comPoint
+        ? "Computer Won !"
+        : "Its a Tie"}
+      {setTimeout(() => {
+        setIsGame(false);
+        setIsGameOn(false);
+      }, 2000)}
+    </>
+  );
+}
+
+function Button({ children, onClick }) {
+  return <button onClick={onClick}>{children}</button>;
+}
